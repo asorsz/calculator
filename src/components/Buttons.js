89430fe47c048460add0446b674985fb0498dc18
asorsz <asorsz@gmail.com>
2021-03-27 17:45:33 +0000

    Add button data and render using data

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,25 +1,16 @@
+import buttonData from './button-data'
 import '../css/Buttons.css';
 
 const Buttons = () => {
   return (
     <div id="buttons">
-      <button class='operator' data-action="add">+</button>
-      <button class='operator' data-action="subtract">-</button>
-      <button class='operator' data-action="multiply">&times;</button>
-      <button class='operator' data-action="divide">&divide;</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>0</button>
-      <button data-action="decimal">.</button>
-      <button data-action="clear">AC</button>
-      <button class="equals" data-action="calculate">=</button>
+      {buttonData.map((button, i) => (
+        <button
+          key={i}
+          id={button.id}
+          className={button.type}
+        >{button.text}</button>
+      ))}
     </div>
   );
 };
---
+++ src/components/button-data.js
@@ -0,0 +1,21 @@
+const buttonData = [
+  { id: 'add', text: '+', type: 'operator' },
+  { id: 'subtract', text: '-', type: 'operator' },
+  { id: 'multiply', text: '\u00D7', type: 'operator' },
+  { id: 'divide', text: '\u00F7', type: 'operator' },
+  { id: 'seven', text: '7', type: 'number' },
+  { id: 'eight', text: '8', type: 'number' },
+  { id: 'nine', text: '9', type: 'number' },
+  { id: 'four', text: '4', type: 'number' },
+  { id: 'five', text: '5', type: 'number' },
+  { id: 'six', text: '6', type: 'number' },
+  { id: 'one', text: '1', type: 'number' },
+  { id: 'two', text: '2', type: 'number' },
+  { id: 'three', text: '3', type: 'number' },
+  { id: 'zero', text: '0', type: 'number' },
+  { id: 'decimal', text: '.', type: 'decimal' },
+  { id: 'clear', text: 'AC', type: 'clear' },
+  { id: 'equals', text: '=', type: 'equals' },
+];
+
+export default buttonData;
